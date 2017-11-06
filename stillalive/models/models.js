af22b31d03aa1431990995821ed2c0552f6873ec
drunkbug <qiang.l@husky.neu.edu>
2017-11-05 22:31:42 -0500

    config server

--- stillalive/models/models.js
+++ stillalive/models/models.js
@@ -8,14 +8,12 @@ module.exports = function () {
     var mongoose = require('mongoose');
     mongoose.disconnect();
 
-    var connectionString = 'mongodb://localhost/stillalive';
-
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+       var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+       var password = process.env.MLAB_PASSWORD_WEBDEV;
+       connectionString = 'mongodb://' + username + ':' + password;
+       connectionString += '@ds249565.mlab.com:49565/heroku_db9n72q4'; 
     }
 
     mongoose.connect(connectionString);
